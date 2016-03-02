b13dc2748a632bf2ea5a185c263443abecea6341
Paul Buonopane <paul+git@earth2me.com>
2016-03-02 11:30:52 -0500

    Add Tuple class

---
+++ src/main/java/Tuple.java
@@ -0,0 +1,17 @@
+public final class Tuple<T, U> {
+    private final T a;
+    private final U b;
+
+    public Tuple(T a, U b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public T getA() {
+        return a;
+    }
+
+    public U getB() {
+        return b;
+    }
+}
